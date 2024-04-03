0e9c105db685238246fc4abb94ebc33486f38c1c
LukaLiegis <luka.liegis@gmail.com>
2024-04-03 20:05:23 +0300

    Commit

--- QT101/utils.py
+++ QT101/utils.py
@@ -1,4 +1,5 @@
 import lzma
+import pandas as pd
 import dill as pickle
 
 
@@ -11,3 +12,21 @@ def load_pickle(path):
 def save_pickle(path, obj):
     with lzma.open(path, "wb") as fp:
         pickle.dump(obj, fp)
+
+class Alpha():
+
+    def __init__(self, insts, dfs, start, end):
+        self.insts = insts
+        self.dfs = dfs
+        self.start = start
+        self.end = end
+
+    def init_portfolio_settings(self, trade_range):
+        portfolio_df = pd.DataFrame(index = trade_range).reset_index().rename(columns = {"index":"datetime"})
+        print(portfolio_df)
+
+    def run_simulation(self):
+        print("Running backtest")
+        date_range = pd.date_range(start = self.start, end = self.end, freq = "1d")
+        portfolio_df = self.init_portfolio_settings(trade_range=date_range)
+        print(date_range)
